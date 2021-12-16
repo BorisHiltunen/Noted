4546602db114df76e80e958116fc516d1e0c1b2e
BorisHiltunen <90689146+BorisHiltunen@users.noreply.github.com>
2021-12-16 15:03:33 +0200

    Add changes

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ class Noted:
         self.down = False
         self.up = False
 
-        self.x = 313
-        self.y = 563
+        self.score = ""
+
+        self.x = 300
+        self.y = 300
+
+        self.note_y = -1000
  
         self.display = pygame.display.set_mode((self.width, self.height))
  
@@ -29,9 +33,32 @@ class Noted:
     #Function that downloads pictures
     def download_pictures(self):
 
-        pass
-        #self.background = pygame.image.load("background.png")
-        #self.button = pygame.image.load("button.png")
+        self.background = pygame.image.load("background.png")
+        self.button1 = pygame.image.load("button1.png")
+        self.button1_hover_over = pygame.image.load("button1_hover_over.png")
+        self.line = pygame.image.load("line.png")
+        self.note = pygame.image.load("note.png")
+
+    def notes(self):
+
+        self.used_note = self.display.blit(self.note, (300, self.note_y))
+
+    def buttons(self):
+
+        self.line_x = 0 >= self.x-self.line.get_width() and 0 <= self.x+self.line.get_width()
+        self.line_y = 500 >= self.y-self.button1.get_height() and 500 <= self.y+self.button1.get_height()
+
+        if self.line_x and self.line_y:
+            if self.right == True:
+                self.used_button = self.display.blit(self.button1_hover_over, (100, 500))
+            if self.left == True:
+                self.used_button = self.display.blit(self.button1_hover_over, (100, 500))
+            if self.up == True:
+                self.used_button = self.display.blit(self.button1_hover_over, (100, 500))
+            if self.down == True:
+                self.used_button = self.display.blit(self.button1_hover_over, (100, 500))
+            else:
+                self.used_button = self.display.blit(self.button1, (100, 500)) 
 
     #Function that loops over and over
     #Thus making the game flow
@@ -39,18 +66,16 @@ class Noted:
         clock = pygame.time.Clock()
 
         while True:
-            self.draw_the_game()
-
             self.analyse_events()
+
+            self.draw_the_game()
 
             clock.tick(60)
 
-            if self.done:
-                self.won_the_game()
-                break
-
     #Function that is mostly responsible for the way the character is moving
     def analyse_events(self):
+
+        self.note_y += 10
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -84,10 +109,14 @@ class Noted:
  
         self.display.fill((0,100,100))
 
-        #self.display.blit(self.background, (0, 0))
+        self.display.blit(self.background, (0, 0))
 
-        self.display.blit(self.button, (self.x, self.y))
- 
+        self.display.blit(self.line, (0, 500))
+
+        self.notes()
+            
+        self.buttons()
+        
         pygame.display.flip()
 
 #Used to execute code if the file is run directly
